a3c26e2a8f85738d5bcae77845bd2fa954fb49e9
GitHub Action <action@github.com>
2022-02-15 10:17:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/felixthec/pycharmplugintraining/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.felixthec.pycharmplugintraining.services
+
+import com.github.felixthec.pycharmplugintraining.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/felixthec/pycharmplugintraining/services/MyProjectService.kt
+++ src/main/kotlin/com/github/felixthec/pycharmplugintraining/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.felixthec.pycharmplugintraining.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.felixthec.pycharmplugintraining.MyBundle
 
 class MyProjectService(project: Project) {
 
